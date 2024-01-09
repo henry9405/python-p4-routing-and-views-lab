a3f367c84aa8f09231c00c28b2e68577f305cd78
henry9405 <charleshenry9405@gmail.com>
2024-01-09 14:19:24 +0300

    new

--- server/app.py
+++ server/app.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    result = '<br>'.join(map(str, range(param)))
+    return result
+
+@app.route('/math/<float:num1>/<operation>/<float:num2>')
+def math(num1, operation, num2):
+    result = None
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = 'Error: Division by zero'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        result = 'Error: Invalid operation'
+
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
